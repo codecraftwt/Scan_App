8bb2ec0164a8ea836e87eadac7c4b5ec4e7d9511
codecraftwt <codecraftwt@gmail.com>
2024-12-09 19:21:00 +0530

    added changes regarding responsive changes

--- src/Utils/inputBox.js
+++ src/Utils/inputBox.js
@@ -1,6 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SvgXml} from 'react-native-svg';
+import {m} from 'walstar-rn-responsive';
 
 const InputBox = ({icon, borderColor}) => {
   return (
@@ -14,7 +15,7 @@ export default InputBox;
 
 const styles = StyleSheet.create({
   svgIcon: {
-    width: 14,
-    height: 31,
+    width: m(14),
+    height: m(31),
   },
 });
